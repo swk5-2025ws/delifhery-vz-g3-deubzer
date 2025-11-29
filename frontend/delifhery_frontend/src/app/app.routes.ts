7a91141bd1807274e1acfd1d9aea05eb8839db07
Mark Deubzer <mark.deubzer@gmail.com>
2025-11-29 20:36:06 +0100

    [Frontend] Started Tracking/id page

--- frontend/delifhery_frontend/src/app/app.routes.ts
+++ frontend/delifhery_frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {Home} from './home/home';
 import {Tracking} from './tracking/tracking';
+import {TrackingDetails} from './tracking-details/tracking-details';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path: 'tracking',
     component: Tracking
+  },
+  {
+    path: 'tracking/:trackingId',
+    component: TrackingDetails
   }
 ];
---
+++ frontend/delifhery_frontend/src/app/tracking-details/tracking-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tracking-details',
+  imports: [],
+  templateUrl: './tracking-details.html',
+  styleUrl: './tracking-details.css',
+})
+export class TrackingDetails {
+
+}
