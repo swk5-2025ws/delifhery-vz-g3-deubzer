3a717221bb50a68bf97482b36a9ea4170dfe38de
Mark Deubzer <mark.deubzer@gmail.com>
2025-12-16 12:56:30 +0100

    [Frontend] Moved frontend files to new repo

--- frontend/delifhery_frontend/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'https://localhost:7156',
-  keycloak: {
-    url: 'http://localhost:8080',
-    realm: 'delifhery',
-    clientId: 'delifhery-spa'
-  }
-};
